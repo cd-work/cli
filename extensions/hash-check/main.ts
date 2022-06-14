58f4e7c8861bf31c4c6a3cef47ab9942d5f9ff7d
Andrea Venuta <andrea@phylum.io>
2022-06-14 10:24:08 +0200

    Compute hashes (deno runtime)

--- extensions/hash-check/main.ts
+++ extensions/hash-check/main.ts
@@ -1,36 +1,46 @@
 // import { readLines } from "https://deno.land/std/io/buffer.ts";
 // import * as log from "https://deno.land/std/log/mod.ts";
 
-let packages = []
+// let packages = []
+// 
+// class PackageDescriptor {
+//   name: string
+//   version: string
+//   package_type: string
+// 
+//   constructor(line: string) {
+//     let fields = line.split(',')
+//     if (fields.length != 3) {
+//       throw new Error(`Could not parse line: ${line}`)
+//     }
+// 
+//     [this.name, this.version, this.package_type] = fields
+//   }
+// }
+// 
+// for await (const line of readLines(Deno.stdin)) {
+//   try {
+//     packages.push(new PackageDescriptor(line))
+//   } catch (e) {
+//     Deno.core.print(e.message)
+//   }
+// }
+// let r = await Deno.core.opAsync("submit_request", packages)
+// Deno.core.print(r)
 
-class PackageDescriptor {
-  name: string
-  version: string
-  package_type: string
+import { walk } from "https://deno.land/std@0.143.0/fs/mod.ts"
+import { crypto } from "https://deno.land/std@0.143.0/crypto/mod.ts"
+import { encode } from "https://deno.land/std@0.143.0/encoding/hex.ts"
 
-  constructor(line: string) {
-    let fields = line.split(',')
-    if (fields.length != 3) {
-      throw new Error(`Could not parse line: ${line}`)
-    }
-
-    [this.name, this.version, this.package_type] = fields
-  }
+async function hashFile(path: string): Promise<string> {
+  let buf = await Deno.readFile(path)
+  let digest = await crypto.subtle.digest("SHA-256", buf)
+  return new TextDecoder().decode(encode(new Uint8Array(digest)))
 }
 
-Deno.core.print('a')
-
-for await (const line of readLines(Deno.stdin)) {
-  try {
-    packages.push(new PackageDescriptor(line))
-  } catch (e) {
-    Deno.core.print(e.message)
-    // log.error(e.message)
+for await (const entry of walk(".")) {
+  if (!entry.isDirectory) {
+    let hash = await hashFile(entry.path)
+    console.log(`${entry.path}: ${hash}`)
   }
 }
-Deno.core.print('b')
-
-// log.info(packages)
-
-let r = await Deno.core.opAsync("submit_request", packages)
-Deno.core.print(r)
